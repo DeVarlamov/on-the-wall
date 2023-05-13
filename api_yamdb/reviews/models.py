bb063396c2191805dddac3c31507402199269c5d
Дмитрий Прокопенко <diozan@inbox.ru>
2023-05-14 01:04:18 +0300

    Составил README.md

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -121,7 +121,7 @@ class Title(models.Model):
     )
     category = models.ForeignKey(
         Category,
-        on_delete=models.SET_NULL,  # Удаление кат-рии не удаляет пр-ние
+        on_delete=models.SET_NULL,
         related_name='titles',
         verbose_name='категория',
         null=True,
@@ -135,7 +135,8 @@ class Title(models.Model):
     )
     genre = models.ManyToManyField(
         Genre,
-        through='GenreTitle',
+        related_name='titles',
+        verbose_name='жанр',
     )
 
     class Meta:
